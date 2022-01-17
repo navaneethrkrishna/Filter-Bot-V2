1a4944dde8d08a3872e61a7679ac0715eafa729f
navaneethrkrishna <95025235+navaneethrkrishna@users.noreply.github.com>
2022-01-17 16:35:42 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
+I'm an unlimited filter bot with more abilities !
 There is no practical limits for my filtering capacity :)
 
 See <i>/help</i> for commands and more details.</b>
